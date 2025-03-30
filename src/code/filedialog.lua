51c5edbb0e4f7d582c2fc908dbda5f026dd1a885
WuffMakesGames <56092018+WuffMakesGames@users.noreply.github.com>
2025-03-29 22:38:00 -0400

    nfd build compatible

--- src/code/filedialog.lua
+++ src/code/filedialog.lua
@@ -1,10 +1,10 @@
 local filedialog = {}
 local pickfile = require("libraries/pickfile")
+local nfd = love.filesystem.isFused() and require("nfd")
 
 function filedialog.get_path()
-    local path = pickfile("Select a file", nil, {
-		"PICO-8", "*.p8"
-	})
+	local path = nfd and nfd.open("p8")
+		or pickfile("Select a file", nil, {"PICO-8", "*.p8"})
 	print(path)
     return path
 end

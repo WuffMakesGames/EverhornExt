3c1a90e8cd8653176f5196e9854034ae3db0b4fc
WuffMakesGames <56092018+WuffMakesGames@users.noreply.github.com>
2025-03-31 13:21:15 -0400

    Fix to potential nfd fix

--- src/code/filedialog.lua
+++ src/code/filedialog.lua
@@ -5,7 +5,7 @@ local nfd = love.filesystem.isFused() and require("nfd")
 function filedialog.get_path(extension,name)
 	local path = nfd and nfd.open(extension)
 		or pickfile("Select a file", nil, {name or "PICO-8", "*."..extension})
-	if nfd then path = path:gsub("^\\", "/") end
+	if nfd then path = path:gsub("%\\", "/") end
 	print(path)
     return path
 end
